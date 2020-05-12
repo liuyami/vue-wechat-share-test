c2ad0b9ce12f6849f30a7e81365b01beb34baab7
Yami <evila@qq.com>
2020-05-12 22:32:34 +0800

    Vue 测试通过

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import WechatJSSDK from 'WechatJSSDK'
+import WechatJSSDK from '@yushuo/wechat-jssdk'
+import Vconsole from 'vconsole'
 
 Vue.config.productionTip = false
 
-let wxJssdk = new WechatJSSDK();
+let vConsole = new Vconsole()
+Vue.use(vConsole)
 
-// Vue.use(wxJssdk);
+let wxJssdk = new WechatJSSDK({
+  debug: true,
+  desc: "测试DEMO，仅供参考",
+  imgUrl:"https://yami.h5.yscase.com/test-wechat/v2/img/logo.82b9c7a5.png"
+});
 Object.defineProperty(Vue.prototype, '$wxJssdk', { value: wxJssdk });
 
 new Vue({
   render: h => h(App),
+
 }).$mount('#app')
